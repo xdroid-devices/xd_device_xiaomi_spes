1122d0dbb1c92bed194dbc7acaee96980d79b59d
LuK1337 <priv.luk@gmail.com>
2020-05-17 17:23:04 +0100

    spes: fingerprint: Make fingerprint HIDL fully treble compliant
    This is more like a hack, also observed in the stock HAL, for pre-P shipped devices
    because those have fpdata store path set to /data/system/users/ by the FingerprintService.
    
    Force treble compliant path and thus avoid using data_between_core_and_vendor_violators
    attribute:
    
     > typeattribute hal_fingerprint_default data_between_core_and_vendor_violators;
     > # access to /data/system/users/[0-9]+/fpdata
     > allow hal_fingerprint_default fingerprintd_data_file:dir rw_dir_perms;
     > allow hal_fingerprint_default fingerprintd_data_file:file create_file_perms;
    
    Change-Id: I388f993de7f95fc68007d945f5a9cc975afde120
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -19,6 +19,7 @@
 
 #include <hardware/hw_auth_token.h>
 
+#include <android-base/strings.h>
 #include <hardware/hardware.h>
 #include <hardware/fingerprint.h>
 #include "BiometricsFingerprint.h"
@@ -47,6 +48,8 @@ constexpr int32_t kBoostDurationMs = 2000;
 
 using RequestStatus =
         android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
+
+using ::android::base::StartsWith;
 
 BiometricsFingerprint *BiometricsFingerprint::sInstance = nullptr;
 
@@ -298,12 +301,18 @@ Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
         ALOGE("Bad path length: %zd", storePath.size());
         return RequestStatus::SYS_EINVAL;
     }
-    if (access(storePath.c_str(), W_OK)) {
+    std::string mutableStorePath = storePath;
+    if (StartsWith(mutableStorePath, "/data/system/users/")) {
+        mutableStorePath = "/data/vendor_de/";
+        mutableStorePath +=
+                static_cast<std::string>(storePath).substr(strlen("/data/system/users/"));
+    }
+    if (access(mutableStorePath.c_str(), W_OK)) {
         return RequestStatus::SYS_EINVAL;
     }
 
     return ErrorFilter(mDevice->set_active_group(mDevice, gid,
-                                                    storePath.c_str()));
+                                                    mutableStorePath.c_str()));
 }
 
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
