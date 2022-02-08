df3b61981ff0c1f0f13ba12ad38b0e9ec871c3ee
xzyuan <xyzuannihboss@gmail.com>
2022-02-08 16:08:17 +0700

    spes: Declare `Redmi Note 11` specs information
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I37f1b8267396ec67effa959a146f449f7262accb

--- device.mk
+++ device.mk
@@ -389,7 +389,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-xd
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
