437798a927d88362a33b86ba5ece216cbefad0ce
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    spes: Migrate to AIDL ClearKey DRM HAL
     * Also label it
    
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- device.mk
+++ device.mk
@@ -247,10 +247,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm-service.clearkey
 
 # Fastboot
 PRODUCT_PACKAGES += \
